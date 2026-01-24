3e5cf8e0ff32b6cf755430133405eb079bda7da2
William Liu <wliu40@calpoly.edu>
2026-01-23 17:39:30 -0800

    task 1,2,3 (up to but not including 3.2)

---
+++ Assignment2/Task1.py
@@ -0,0 +1,54 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+
+q_literal = "B10B8F96 A080E01D DE92DE5E AE5D54EC 52C99FBC FB06A3C6 9A6A9DCA 52D23B61 6073E286 75A23D18 9838EF1E 2EE652C0 13ECB4AE A9061123 24975C3C D49B83BF ACCBDD7D 90C4BD70 98488E9C 219A7372 4EFFD6FA E5644738 FAA31A4F F55BCCC0 A151AF5F 0DC8B4BD 45BF37DF 365C1A65 E68CFDA7 6D4DA708 DF1FB2BC 2E4A4371"
+g_literal = "A4D1CBD5 C3FD3412 6765A442 EFB99905 F8104DD2 58AC507F D6406CFF 14266D31 266FEA1E 5C41564B 777E690F 5504F213 160217B4 B01B886A 5E91547F 9E2749F4 D7FBD7D3 B9A92EE1 909D0D22 63F80A76 A6A24C08 7A091F53 1DBF0A01 69B6A28A D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24 855E6EEB 22B3B2E5"
+
+q_literal = q_literal.replace(" ", "")
+g_literal = g_literal.replace(" ", "")
+
+q = int(q_literal, 16)
+g = int(g_literal, 16)
+
+def get_public_key(secret):
+    return pow(g, secret, q)
+
+def get_shared_key(secret, other_public):
+    return pow(other_public,secret, q)
+
+x_a = 30
+x_b = 123
+
+public_key_a = get_public_key(x_a)
+public_key_b = get_public_key(x_b)
+
+shared_key_a = get_shared_key(x_a, public_key_b)
+shared_key_b = get_shared_key(x_b, public_key_a)
+
+print(shared_key_a == shared_key_b)
+assert shared_key_a == shared_key_b
+
+message = pad(str.encode("encrypt this message"),16)
+
+truncated_key = str(shared_key_a)[:16].encode()
+
+iv = get_random_bytes(16)
+
+cipher_a = AES.new(truncated_key, AES.MODE_CBC,iv=iv)
+cipher_b = AES.new(truncated_key, AES.MODE_CBC,iv=iv)
+
+encrypted_message_a = cipher_a.encrypt(message)
+encrypted_message_b = cipher_b.encrypt(message)
+
+decipher_a = AES.new(truncated_key, AES.MODE_CBC,iv=iv)
+decipher_b = AES.new(truncated_key, AES.MODE_CBC,iv=iv)
+
+decrypted_message_a = unpad(decipher_a.decrypt(encrypted_message_a),16)
+decrypted_message_b = unpad(decipher_b.decrypt(encrypted_message_b),16)
+
+print(decrypted_message_a == decrypted_message_b)
+assert decrypted_message_a == decrypted_message_b
+
+
+
---
+++ Assignment2/Task3.py
@@ -0,0 +1,51 @@
+from Crypto.Util import number
+
+# Provided by Yocam
+def mod_inverse(a, m):
+    def egcd(a,b):
+        if a == 0: return b, 0, 1
+        else:
+            g,y,x = egcd(b % a, a)
+            return g, x - (b // a) * y, y
+    g,x, _ = egcd(a,m)
+    if g != 1: raise Exception('modular inverse does not exist')
+    else: return x % m
+
+def generate_keys(bits_prime_length):
+    if bits_prime_length > 2048:
+        raise TypeError("Bit length must be <= 2048")
+    p = number.getPrime(bits_prime_length)
+    q = number.getPrime(bits_prime_length)
+    n = p * q
+    phi = (p-1) * (q-1)
+    e = 65537
+    d = mod_inverse(e, phi)
+
+    return n,e,d
+
+def encrypt(plaintext, e, n):
+    m = int(plaintext.encode('ascii').hex(),16)
+    return pow(m,e,n)
+def decrypt(cipher_int, d, n):
+    m = pow(cipher_int, d, n)
+    return bytes.fromhex(hex(m)[2:]).decode('ASCII')
+
+keys = generate_keys(128)
+n = keys[0]
+e = keys[1]
+d = keys[2]
+
+message = "Hello this is the message"
+
+encrypted_int = encrypt(message, e, n)
+decrypted_message = decrypt(encrypted_int, d, n)
+
+assert decrypted_message == message
+print(decrypted_message)
+
+
+
+
+
+
+
